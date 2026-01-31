ad99dfec10d8c20667ebdbca59f1e204608bc224
Nurbol <kadyrbekovnurbol002@gmail.com>
2026-01-31 14:00:22 +0600

    Озгортуу киргиздим

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, product_detail, product_delete
 
 urlpatterns = [
-    path('products/', product_list)
+    path('products/', product_list),
+    path('products/<int:pk>/', product_detail),
+    path('products/<int:pk>/delete/', product_delete)
 ]
--- products/views.py
+++ products/views.py
@@ -5,8 +5,45 @@ from .serializers import ProductSerializer
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+@api_view(['GET', 'PUT'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        return Response(status=404)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+
+def product_delete(request, pk):
+    try:
+        product = Product.objects.get(id=pk)
+    except Product.DoesNotExist:
+        return Response(status=404)
+    product.delete()
+    return Response(status=204)
